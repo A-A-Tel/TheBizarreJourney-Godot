62e3ce3a2982fc50e1731266ecb4738746200c07
a-a-tel <anouaranthonytel@gmail.com>
2025-06-05 15:29:07 +0200

    Added the basic main menu diagram and model

--- Scripts/UI/MainMenu.cs
+++ Scripts/UI/MainMenu.cs
@@ -2,7 +2,18 @@ using Godot;
 
 namespace TheBizarreJourney.Scripts.UI;
 
-public partial class MainMenu : Node
+public partial class MainMenu : Control
 {
-    
+	private Button _startButton;
+
+	private void StartGame()
+	{
+		GD.Print("Game started");
+	}
+
+	public override void _Ready()
+	{
+		_startButton = GetNode<Button>("StartButton");
+		_startButton.Pressed += StartGame;
+	}
 }
